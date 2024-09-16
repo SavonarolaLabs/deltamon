2ad339c45d4056065f7cb510af7be0b014c95d55
c8e4 <c8e4@proton.me>
2024-09-16 22:10:43 +0200

    if hp==0 die

--- src/lib/pvp/combat.ts
+++ src/lib/pvp/combat.ts
@@ -13,5 +13,17 @@ export function attack(
 	)?.creature!;
 	const attackDmg = Math.max(source.attack - target.defense, 0);
 	target.hp = Math.max(0, target.hp - attackDmg);
+	if (target.hp == 0) {
+		die(game, targetId);
+	}
 	return game;
+}
+
+// if creature dies but returns dmg and then the other dies, the last one to die wins
+export function die(game: GameState, creatureId: BattleCreatureId): GameState {
+	game.slots.forEach((slot) => {
+		if (slot.creature?.bcId == creatureId) {
+			slot.creature = null;
+		}
+	});
 }
