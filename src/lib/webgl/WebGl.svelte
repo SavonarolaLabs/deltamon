b1de42f0797a4a7a1df8588e4620927330416865
c8e4 <c8e4@proton.me>
2024-09-20 15:46:40 +0200

    enable blend

--- src/lib/webgl/WebGl.svelte
+++ src/lib/webgl/WebGl.svelte
@@ -8,18 +8,19 @@
 	function initWebGL() {
 		let gl = canvas.getContext("webgl");
 		if (gl) {
-			gl.clearColor(1.0, 1.0, 0.0, 1.0);
-			//gl.clear(gl.COLOR_BUFFER_BIT);
+			gl.enable(gl.BLEND);
+			gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+			gl.clearColor(1, 1, 0, 1);
 
-			const vertShader = gl.createShader(gl.VERTEX_SHADER);
+			const vertShader = gl.createShader(gl.VERTEX_SHADER)!;
 			gl.shaderSource(vertShader, vertCode);
 			gl.compileShader(vertShader);
 
-			const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+			const fragShader = gl.createShader(gl.FRAGMENT_SHADER)!;
 			gl.shaderSource(fragShader, fragCode);
 			gl.compileShader(fragShader);
 
-			const shaderProgram = gl.createProgram();
+			const shaderProgram = gl.createProgram()!;
 			gl.attachShader(shaderProgram, vertShader);
 			gl.attachShader(shaderProgram, fragShader);
 			gl.linkProgram(shaderProgram);
@@ -28,9 +29,7 @@
 			const vertices = new Float32Array([
 				0.0, 1.0, -1.0, -1.0, 1.0, -1.0,
 			]);
-
-			const buffer = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
 			gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
 			const coord = gl.getAttribLocation(shaderProgram, "coordinates");
