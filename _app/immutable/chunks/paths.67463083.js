33a8405d461af28233c043281b0373abcc933ee0
c8e4 <c8e4@proton.me>
2024-09-30 18:42:52 +0200

    Updates

--- _app/immutable/chunks/paths.67463083.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1wr4bpt)==null?void 0:s.base)??"/deltamon";var t;const e=((t=globalThis.__sveltekit_1wr4bpt)==null?void 0:t.assets)??a;export{e as a,a as b};
